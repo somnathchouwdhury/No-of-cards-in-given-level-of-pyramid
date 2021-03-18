a2da13136772895509c9eede6fff6302a9fd07a0
somnathchouwdhury <76212214+somnathchouwdhury@users.noreply.github.com>
2021-03-18 21:55:53 +0530

    Update NoOfCards.py

--- NoOfCards.py
+++ NoOfCards.py
@@ -10,14 +10,16 @@ def CardsPyramid(n):
     cards=0
     level=0
     
-    
+#we must return -1 if the level specified is 0     
     if(n==0):
         return -1
     
-    
+#condition for levels other than 0    
     else:
         for i in range(1,n+1):
-            cards=cards+(i*2)
+            #this would store No of cards in each level
+            cards=cards+(i*2) 
+            #this would store the no of cards to support the pyramid i.e. cards in between two rows
             level=level+(i-1)
             
         NoOfCards=cards+level
